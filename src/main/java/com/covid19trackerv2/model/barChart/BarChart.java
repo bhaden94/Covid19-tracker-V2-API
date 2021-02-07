42181786d1d5b51c6f59c0e12b1a32a95ea17520
bhaden94 <bhaden94@yahoo.com>
2021-02-07 12:21:42 -0800

    add model and routes for bar chart data

---
+++ src/main/java/com/covid19trackerv2/model/barChart/BarChart.java
@@ -0,0 +1,41 @@
+package com.covid19trackerv2.model.barChart;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BarChart {
+    private String rate;
+    @JsonProperty("state_country")
+    private Double stateCountry;
+    @JsonProperty("us_world")
+    private Double usWorld;
+
+    public BarChart(String rate) {
+        this.rate = rate;
+        this.stateCountry = 0.0;
+        this.usWorld = 0.0;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public Double getStateCountry() {
+        return stateCountry;
+    }
+
+    public void setStateCountry(Double stateCountry) {
+        this.stateCountry = stateCountry;
+    }
+
+    public Double getUsWorld() {
+        return usWorld;
+    }
+
+    public void setUsWorld(Double usWorld) {
+        this.usWorld = usWorld;
+    }
+}
